a63dd14fd3ed0d8a6bddd0ce2d6137b4ff00540a
ykkssyaa <mr.ender.03@mail.ru>
2023-01-08 03:52:13 +0300

    add onClick method add UdpateMoney method money adding per second

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,10 +6,33 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField]
-    private int money;
+    private int p_money;
+    [SerializeField]
+    private int p_moneyPerSecond = 0;
 
     private void Start()
     {
-        money = 0;
+        p_money = 0;
+        StartCoroutine(AddMoneyPerSecond());
     }
+
+    IEnumerator AddMoneyPerSecond()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(1);
+            UpdateMoney(p_moneyPerSecond);
+        }
+    }
+
+    public void OnClick()
+    {
+        UpdateMoney(1);
+    }
+
+    private void UpdateMoney(int value)
+    {
+        p_money+= value;
+        // Update UI
+    } 
 }
